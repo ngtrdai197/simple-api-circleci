6a698cd25970805e30c2b26236dd1ce220a138bd
Dai Nguyen <ngtrdai290197@gmail.com>
2021-10-19 12:23:38 +0700

    Add logs debug

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,18 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
   getHello(): string {
+    this.logger.debug('Hello func');
     return 'Hello World!';
   }
 
   public getMe() {
+    this.logger.debug('getMe func');
     return {
       name: 'Dai Nguyen Update',
       phone: '0375629888',
       age: 24,
       city: 'HCMC',
-      gender: "Male"
+      gender: 'Male',
     };
   }
 }
